a32080e969e7bd0b66f3e5fea273094513143ef9
Personal <ibneshakeel34@gmail.com>
2024-10-28 08:14:40 -0700

    'basic'

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Bappy",
+    author_email="entbappy73@gmail.com",
+    packages=find_packages()
+)
+
+
